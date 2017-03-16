60192399380949abdc127d960d3f5c9a9d9ee8fa
Richard Olsson <r@richardolsson.se>
2017-03-16 13:23:35 +0100

    Add news to front page

--- troja/front-page.php
+++ troja/front-page.php
@@ -1,21 +1,40 @@
 <?php get_header();?>
 <?php
-    $posts = get_posts(array(
+    $first = true;
+    $projects = get_posts(array(
         'post_type' => 'project',
     ));
 ?>
 <div class="content front">
-<?php foreach ($posts as $post):
-    $img_src = get_the_post_thumbnail_url($post);
+<?php foreach ($projects as $project):
+    $img_src = get_the_post_thumbnail_url($project);
 ?>
     <div class="project" style="background-image: url(<?php echo $img_src ?>)">
         <div class="project-meta">
-            <h2><?php echo $post->post_title;?></h2>
+            <h2><?php echo $project->post_title;?></h2>
             <p class="excerpt">
-                <?php echo $post->post_excerpt;?>
+                <?php echo $project->post_excerpt;?>
             </p>
         </div>
     </div>
+
+    <?php if ($first):
+        $first = false;
+        $news = get_posts(array(
+            'post_type' => 'post',
+            'posts_per_page' => 3,
+        ));
+    ?>
+        <div class="news">
+        <?php foreach ($news as $post):?>
+            <a href="<?php echo get_permalink($post);?>" class="news-item">
+                <small class="date"><?php echo get_the_date(null, $post->ID);?></small>
+                <h3><?php echo $post->post_title;?></h3>
+            </a>
+        <?php endforeach; ?>
+        </div>
+
+    <?php endif;?>
 <?php endforeach;?>
 </div>
 <?php get_footer();?>
