7e04cbcfe9c0e96a39767b1555ee0e3176bfa02e
Richard Olsson <r@richardolsson.se>
2017-03-23 16:02:54 +0100

    Add favicon
    Fixes #1

--- troja/header.php
+++ troja/header.php
@@ -4,6 +4,7 @@
         <title><?php the_title();?></title>
         <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/main.css">
         <link href="https://fonts.googleapis.com/css?family=Poppins:500,600|Taviraj:400i" rel="stylesheet">
+        <link rel="icon" href="<?php echo get_template_directory_uri();?>/images/favicon.png">
         <meta name="viewport" content="width=device-width">
     </head>
     <body>
